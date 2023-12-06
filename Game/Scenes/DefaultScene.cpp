f09caa686b928176d334ae4f1e67b7cd327bf086
unknown <agathelier@gaming.tech>
2023-12-06 23:03:38 +0100

    Mur map quasi fini

--- Game/Scenes/DefaultScene.cpp
+++ Game/Scenes/DefaultScene.cpp
@@ -24,6 +24,10 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 26 * sizeWallX, 21 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 21 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 28 * sizeWallX, 21 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 29 * sizeWallX, 21 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 30 * sizeWallX, 21 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 31 * sizeWallX, 21 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 21 * sizeWallY);
 
     //Ligne 2
     SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 20 * sizeWallY);
@@ -34,20 +38,26 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 20 * sizeWallX, 20 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 21 * sizeWallX, 20 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 22 * sizeWallX, 20 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 20 * sizeWallY);
 
     //Ligne 3
     SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 19 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 3 * sizeWallX, 19 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 22 * sizeWallX, 19 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 19 * sizeWallY);
 
     //Ligne 4
     SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 18 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 24 * sizeWallX, 18 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 19 * sizeWallY);
 
     //Ligne 5
     SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 17 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 26 * sizeWallX, 17 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 17 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 28 * sizeWallX, 17 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 29 * sizeWallX, 17 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 17 * sizeWallY);
 
     //Ligne 6
     SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 16 * sizeWallY);
@@ -59,6 +69,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 17 * sizeWallX, 16 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 18 * sizeWallX, 16 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 16 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 16 * sizeWallY);
 
     //Ligne 7 
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 15 * sizeWallY);
@@ -70,6 +81,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 19 * sizeWallX, 15 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 20 * sizeWallX, 15 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 15 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 15 * sizeWallY);
 
     //Ligne 8
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 14 * sizeWallY);
@@ -80,12 +92,19 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 25 * sizeWallX, 14 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 26 * sizeWallX, 14 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 14 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 14 * sizeWallY);
 
     //Ligne 9
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 13 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 27 * sizeWallX, 13 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 28 * sizeWallX, 13 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 13 * sizeWallY);
 
     //Ligne 10
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 12 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 30 * sizeWallX, 12 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 31 * sizeWallX, 12 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 12 * sizeWallY);
 
     //Ligne 11
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 11 * sizeWallY);
@@ -96,6 +115,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 10 * sizeWallX, 11 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 11 * sizeWallX, 11 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 11 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 11 * sizeWallY);
 
     //Ligne 12
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 10 * sizeWallY);
@@ -104,6 +124,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 3 * sizeWallX, 10 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 6 * sizeWallX, 10 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 10 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 10 * sizeWallY);
 
     //Ligne 13
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 9 * sizeWallY);
@@ -111,6 +132,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 9 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 18 * sizeWallX, 9 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 19 * sizeWallX, 9 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 9 * sizeWallY);
 
     //Ligne 14
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 8 * sizeWallY);
@@ -118,10 +140,14 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 7 * sizeWallX, 8 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 15 * sizeWallX, 8 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 16 * sizeWallX, 8 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 8 * sizeWallY);
 
     //Ligne 15
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 7 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 8 * sizeWallX, 7 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 28 * sizeWallX, 7 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 29 * sizeWallX, 7 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 7 * sizeWallY);
 
     //Ligne 16
     SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 6 * sizeWallY);
@@ -134,6 +160,7 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 10 * sizeWallX, 6 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 11 * sizeWallX, 6 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 6 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 6 * sizeWallY);
 
     //Ligne 17
     SpawnWall::CreateWallObject(this, WallName, 6 * sizeWallX, 5 * sizeWallY);
@@ -142,17 +169,49 @@ void DefaultScene::CreateWalls() {
     SpawnWall::CreateWallObject(this, WallName, 14 * sizeWallX, 5 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 15 * sizeWallX, 5 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 16 * sizeWallX, 5 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 5 * sizeWallY);
 
     //Ligne 18
-    SpawnWall::CreateWallObject(this, WallName, 7 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 1 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 2 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 3 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 4 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 5 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 6 * sizeWallX, 4 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 4 * sizeWallY);
 
     //Ligne 19
+    SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 3 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 7 * sizeWallX, 3 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 8 * sizeWallX, 3 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 9 * sizeWallX, 3 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 3 * sizeWallY);
 
     //Ligne 20
+    SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 2 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 10 * sizeWallX, 2 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 2 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 13 * sizeWallX, 2 * sizeWallY);
     SpawnWall::CreateWallObject(this, WallName, 14 * sizeWallX, 2 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 28 * sizeWallX, 2 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 29 * sizeWallX, 2 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 30 * sizeWallX, 2 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 31 * sizeWallX, 2 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 32 * sizeWallX, 2 * sizeWallY);
+
+    //Ligne 21
+    SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 1 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 1 * sizeWallY);
+
+    //Ligne 22
+    SpawnWall::CreateWallObject(this, WallName, 0 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 1 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 4 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 5 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 6 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 9 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 10 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 11 * sizeWallX, 0 * sizeWallY);
+    SpawnWall::CreateWallObject(this, WallName, 12 * sizeWallX, 0 * sizeWallY);
 }
