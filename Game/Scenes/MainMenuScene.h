0b2ab43e9e324b7e6a0a669ce83f97e0009b4798
Dranemo <ycaillot@gaming.tech>
2023-12-06 22:31:20 +0100

    FixMenu

--- Game/Scenes/MainMenuScene.h
+++ Game/Scenes/MainMenuScene.h
@@ -15,6 +15,5 @@ public:
 		GameObject* option_game_button = CreateButton(ButtonName, Maths::Vector2f((window_size.x / 2) - (window_size.x / 15), (window_size.y - window_size.y * 4 / 6)), sf::Color::Blue, Maths::Vector2u(window_size.x / 15 * 2, window_size.y / 10), "option_game_button", "Option");
 		GameObject* credit_game_button = CreateButton(ButtonName, Maths::Vector2f((window_size.x / 2) - (window_size.x / 15), (window_size.y - window_size.y * 3 / 6)), sf::Color::Blue, Maths::Vector2u(window_size.x / 15 * 2, window_size.y / 10), "credit_game_button", "Credit");
 		GameObject* quit_game_button = CreateButton(ButtonName, Maths::Vector2f((window_size.x / 2) - (window_size.x / 15), (window_size.y - window_size.y * 1 / 6)), sf::Color::Red, Maths::Vector2u(window_size.x / 15 * 2, window_size.y / 10), "quit_game_button", "Quit");
-
 	}
 };
