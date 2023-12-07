b8c3176eea6b987bcd550bfb4fb83032ea25e449
unknown <echoquet@gamingcampus.local>
2023-12-07 13:57:20 +0100

    regit

--- SFMLEngine/src/Components/Button.cpp
+++ SFMLEngine/src/Components/Button.cpp
@@ -5,9 +5,21 @@
 
 
 void Button::Execute() {
-	if (GetOwner()->GetName() == "capacity_button") {
-		if (is_clicked) {
-			Scene* scene = Engine::GetInstance()->GetModuleManager()->GetModule<SceneModule>()->GetMainScene();
+	if (is_clicked) {
+		Scene* scene = Engine::GetInstance()->GetModuleManager()->GetModule<SceneModule>()->GetMainScene();
+		if (GetOwner()->GetName() == "doll_button1" && GetOwner()->GetName() == "doll_button2" && GetOwner()->GetName() == "doll_button3") {
+			bool have_capacity_button = false;
+			for (GameObject* const& gameObject : *scene->GetGameObjects()) {
+				if (gameObject->GetName() == "capacity_button") {
+					have_capacity_button = true;
+					break;
+				}
+			}
+			if (!have_capacity_button) {
+
+			}
+		}
+		else if (GetOwner()->GetName() == "capacity_button") {
 			scene->FindGameObject("text_for_capacity_name")->GetComponent<TextRenderer>()->SetString(GetObject()->GetName());
 			scene->FindGameObject("text_for_capacity_description")->GetComponent<TextRenderer>()->SetString(GetObject()->GetDescription());
 		}
