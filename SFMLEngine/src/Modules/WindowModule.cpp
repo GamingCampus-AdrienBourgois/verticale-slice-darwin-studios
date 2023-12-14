a7f2b84e7c6e542e0a0f3251fff8c30efd64cabf
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2023-12-15 00:15:39 +0100

    collisions

--- SFMLEngine/src/Modules/WindowModule.cpp
+++ SFMLEngine/src/Modules/WindowModule.cpp
@@ -83,8 +83,13 @@ void WindowModule::Update()
 					}
 				}
 			}if (event.mouseButton.button == sf::Mouse::Right) {
-				sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
-				std::cout << mousePos.x << " " << mousePos.y << std::endl;
+				mousePos.push_back(sf::Mouse::getPosition(*window));
+				float sizeX = GetWindowSize().x / 100;
+				float sizeY = GetWindowSize().y / 100;
+				std::cout << "r = "<<  mousePos.size() <<std::endl;
+				if(mousePos.size() % 2 == 0){
+					std::cout << mousePos[0].x / sizeX << " " << mousePos[0].y / sizeY << " " << (mousePos[1].x - mousePos[0].x) / sizeX << " " << (mousePos[1].y - mousePos[0].y) / sizeY << std::endl;
+				}
 			}
 		}
 	}
