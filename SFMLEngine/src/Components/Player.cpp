936267401fc7bb813fe72745118eb90c51e30fbf
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2023-12-19 13:15:54 +0100

    jump boing boing

--- SFMLEngine/src/Components/Player.cpp
+++ SFMLEngine/src/Components/Player.cpp
@@ -60,35 +60,20 @@ void Player::Jump(const float _delta_time, std::unordered_map<sf::Keyboard::Key,
 
 	if (is_jumping && GetOwner()->GetComponent<SquareCollider>()->GetCanMoving()["up"]) {
 		if (jumping_time.getElapsedTime().asSeconds() <= 0.4) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (500 * _delta_time)));
-		}
-		else if (jumping_time.getElapsedTime().asSeconds() <= 0.45) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (400 * _delta_time)));
+		GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (window_size.y * 0.3 * _delta_time)));
 		}
 		else if (jumping_time.getElapsedTime().asSeconds() <= 0.5) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (300 * _delta_time)));
-		}
-		else if (jumping_time.getElapsedTime().asSeconds() <= 0.55) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (150 * _delta_time)));
-		}
-		else if (jumping_time.getElapsedTime().asSeconds() <= 0.575) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (75 * _delta_time)));
-		}
-		else if (jumping_time.getElapsedTime().asSeconds() <= 0.585) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (35 * _delta_time)));
-		}
-		else if (jumping_time.getElapsedTime().asSeconds() <= 0.595) {
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (0 * _delta_time)));
+		GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() - (window_size.y * 0 * _delta_time)));
 		}
 		else{
-			GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() + (200 * _delta_time)));
-			can_jump = false;
+		GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() + (window_size.y * 0.3 * _delta_time)));
+		can_jump = false;
 		}
 	}
 
-	if (GetOwner()->GetPosition().GetY() + 200 <= sizeWindow.y && !is_jumping && GetOwner()->GetComponent<SquareCollider>()->GetCanMoving()["down"]) {
-		GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() + (200 * _delta_time)));
-	}
+if (GetOwner()->GetPosition().GetY() + 200 <= sizeWindow.y && !is_jumping && GetOwner()->GetComponent<SquareCollider>()->GetCanMoving()["down"]) {
+	GetOwner()->SetPosition(Maths::Vector2f(GetOwner()->GetPosition().GetX(), GetOwner()->GetPosition().GetY() + (window_size.y * 0.1 * _delta_time)));
+}
 
 }
 
